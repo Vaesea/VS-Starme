9eb71935a8bd2a4e87e7e48c1b3b576b3b9fd8cf
Vaesea <159406785+Vaesea@users.noreply.github.com>
2025-08-08 21:15:04 +0100

    Update MainMenuState.hx
    i thought i already did this

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -14,7 +14,7 @@ enum MainMenuColumn {
 
 class MainMenuState extends MusicBeatState
 {
-	public static var psychEngineVersion:String = '0.0.2'; // This is also used for Discord RPC
+	public static var psychEngineVersion:String = '1.0.0'; // This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 	public static var curColumn:MainMenuColumn = CENTER;
 	var allowMouse:Bool = false; //Turn this off to block mouse movement in menus
@@ -93,7 +93,7 @@ class MainMenuState extends MusicBeatState
 			rightItem.x -= rightItem.width;
 		}
 
-		var psychVer:FlxText = new FlxText(12, FlxG.height - 44, 0, "Stev's Limbo v" + psychEngineVersion, 12);
+		var psychVer:FlxText = new FlxText(12, FlxG.height - 44, 0, "VS Starme v" + psychEngineVersion, 12);
 		psychVer.scrollFactor.set();
 		psychVer.setFormat(Paths.font("comic.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(psychVer);
